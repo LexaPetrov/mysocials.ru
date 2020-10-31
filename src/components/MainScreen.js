5bbe8297c0f038967cd24dfa61ef9f241fe4f83d
lexapetrov <al1111997@yandex.ru>
2020-10-31 12:39:46 +0500

    captcha

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -6,6 +6,7 @@ import Input from './Input'
 import { Redirect } from "react-router-dom"
 import Icon from "./Icon"
 import { backgrounds } from '../utils/backgrounds'
+import Recaptcha from 'react-recaptcha'
 
 const MainScreen = props => {
     const [state, dispatch] = useReducer(reducer, {})
@@ -16,6 +17,7 @@ const MainScreen = props => {
     const [formloginstate, setFormloginstate] = useState({
         username_login: '', password_login: ''
     })
+    const [captcha, setCaptcha] = useState(true)
 
     const onSubmitRegister = async (e) => {
         e.preventDefault();
@@ -49,7 +51,19 @@ const MainScreen = props => {
 
     useEffect(() => {
         actions.get_count(dispatch)
+
     }, [])
+
+    const onLoadCaptcha = () => {
+        setCaptcha(false)
+    }
+
+    const verifyCallback = res => {
+        if (res) {
+            setCaptcha(true)
+        }
+    }
+
     if (state.isLoading) return <Loader />;
     return (
         <>
@@ -66,7 +80,7 @@ const MainScreen = props => {
                 margin: 'auto',
                 paddingTop: '20px',
                 fontSize: '20px'
-            }} text={`Нас уже ${state.count !== undefined && state.count['COUNT(*)']}!`} />
+            }} text={state.count !== undefined ? `Нас уже ${state.count['COUNT(*)']}!` : null} />
             <div className="main__layout__wrapper-content">
 
                 <div className="main__layout__wrapper-content__left">
@@ -77,13 +91,14 @@ const MainScreen = props => {
 
                 </div>
                 <div className='mainscreen__content'>
-                    <p style={{ textAlign: 'center' }}>{!loginBtn ? 'Регистрация' : 'Авторизация'}</p>
                     {
                         !loginBtn && <form onSubmit={onSubmitRegister}>
+                            <p style={{ textAlign: 'center' }}>{!loginBtn ? 'Регистрация' : 'Авторизация'}</p>
+
                             <Input inputplaceholder='username' type='text' required value={formstate.username} onChange={e => onFormChange(e)} name='username' />
                             <Input inputplaceholder='email' type='email' required value={formstate.email} onChange={e => onFormChange(e)} name='email' />
                             <Input inputplaceholder='password' type='password' required value={formstate.password} onChange={e => onFormChange(e)} name='password' minLength='8' />
-                            <button type='submit' className='button button-success register_login' disabled={[formstate.username, formstate.email, formstate.password].some(v => v.length === 0)} >Создать аккаунт</button>
+                            <button type='submit' className='button button-success register_login' disabled={captcha === false || [formstate.username, formstate.email, formstate.password].some(v => v.length === 0)} >Создать аккаунт</button>
                             {
                                 state.success !== undefined && !state.success && <Icon size='14' style={{
                                     display: 'flex',
@@ -91,13 +106,26 @@ const MainScreen = props => {
                                     alignItems: 'center',
                                 }} type='cross' text='данный username занят' />
                             }
+                            <div style={{ alignSelf: 'center' }}>
+                                <Recaptcha
+                                    sitekey="6Lcsc90ZAAAAABI13YttPvIZDEyXZ-ij36rg5W7F"
+                                    render="explicit"
+                                    verifyCallback={verifyCallback}
+                                    onloadCallback={onLoadCaptcha}
+                                    theme={window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'}
+                                    size='compact'
+                                    hl='ru'
+                                />
+                            </div>
                         </form>
                     }
                     {
                         loginBtn && <form onSubmit={onSubmitLogin}>
+                            <p style={{ textAlign: 'center' }}>{!loginBtn ? 'Регистрация' : 'Авторизация'}</p>
+
                             <Input inputplaceholder='username' type='text' required value={formloginstate.username_login} onChange={e => onFormLoginChange(e)} name='username_login' />
                             <Input inputplaceholder='password' type='password' required value={formloginstate.password_login} onChange={e => onFormLoginChange(e)} name='password_login' minLength='8' />
-                            <button type='submit' className='button button-success register_login' disabled={[formloginstate.username_login, formloginstate.password_login].some(v => v.length === 0)} >Войти</button>
+                            <button type='submit' className='button button-success register_login' disabled={captcha === false || [formloginstate.username_login, formloginstate.password_login].some(v => v.length === 0)} >Войти</button>
                             {
                                 state.success !== undefined && !state.success && <Icon size='14' style={{
                                     display: 'flex',
@@ -105,6 +133,17 @@ const MainScreen = props => {
                                     alignItems: 'center',
                                 }} type='cross' text='неверный логин или пароль' />
                             }
+                            <div style={{ alignSelf: 'center' }}>
+                                <Recaptcha
+                                    sitekey="6Lcsc90ZAAAAABI13YttPvIZDEyXZ-ij36rg5W7F"
+                                    render="explicit"
+                                    verifyCallback={verifyCallback}
+                                    onloadCallback={onLoadCaptcha}
+                                    theme={window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'}
+                                    size='compact'
+                                    hl='ru'
+                                />
+                            </div>
                             {
                                 state.success && state.id !== undefined && <Redirect to={{
                                     pathname: "/settings",
@@ -122,12 +161,12 @@ const MainScreen = props => {
                     }} onClick={() => setLoginBtn(!loginBtn)} text={loginBtn ? 'Нет аккаунта? Создать' : 'Есть аккаунт? Войти'} />
                     <div className="left-links main__screen">
                         <p><a href='/privacy'>Конфиденциальность</a></p>
-                        <p><a href='/'>v1.0.5</a></p>
+                        {/* <p><a href='/'>v1.0.5</a></p> */}
                         {/* <p><a href='/'><img style={{ width: '170px', height: '50px' }} src='https://www.centrinvest.ru/images/mirpay/mp-gp.png' alt='img' /></a></p> */}
                         <br></br>
                         <br></br>
                         <br></br>
-                        <p><a href='https://heroku.com'>powered by Heroku</a></p>
+                        {/* <p><a href='https://heroku.com'>powered by Heroku</a></p> */}
                         {/* <p><a href='https://twitter.com/admin'>mysocials.ru</a></p> */}
                     </div>
                 </div>
